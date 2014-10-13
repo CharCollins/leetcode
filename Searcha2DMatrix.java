b8e0ee7311eb26d9c15857caa602a531cd9f78ae
CharCollins <unique.zhangliang@gmail.com>
2014-10-13 15:44:11 +0800

    https://oj.leetcode.com/problems/search-a-2d-matrix/
    Solution:
    http://fisherlei.blogspot.com/2013/03/leetcode-search-2d-matrix-solution.html

---
+++ Searcha2DMatrix.java
@@ -0,0 +1,11 @@
+public class Searcha2DMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row=matrix.length,col=matrix[0].length;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(matrix[i][j]==target) return true;
+            }
+        }
+        return false;
+    }
+}
