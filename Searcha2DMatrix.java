35ee3124523ac6ec7e56bec266c7a7d4a4093a6c
CharCollins <unique.zhangliang@gmail.com>
2014-10-13 16:02:19 +0800

    Add comment

--- Searcha2DMatrix.java
+++ Searcha2DMatrix.java
@@ -2,7 +2,8 @@ public class Searcha2DMatrix {
     public boolean searchMatrix(int[][] matrix, int target) {
         int row=matrix.length,col=matrix[0].length;
         if(row==0||col==0) return false;
-        if(target<matrix[0][0]) return false;
+        if(target<matrix[0][0]) return false;//不判断情况下，第一个循环中实行到end=mid-1；则end为-1，第二个循环数组下标越界
+        //target>matrix[row-1][col-1] 可以不判断，因为两个while循环不满足，不越界，最后函数会返回FALSE
         int start=0,end=row-1,mid=0;
         while(start<=end){
             mid=(start+end)/2;
