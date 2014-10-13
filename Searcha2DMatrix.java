74c506aa0147542e7d6e5c5ba1913a5270da6b1a
CharCollins <unique.zhangliang@gmail.com>
2014-10-13 15:59:45 +0800

    Add binary search method

--- Searcha2DMatrix.java
+++ Searcha2DMatrix.java
@@ -1,11 +1,32 @@
 public class Searcha2DMatrix {
     public boolean searchMatrix(int[][] matrix, int target) {
         int row=matrix.length,col=matrix[0].length;
+        if(row==0||col==0) return false;
+        if(target<matrix[0][0]) return false;
+        int start=0,end=row-1,mid=0;
+        while(start<=end){
+            mid=(start+end)/2;
+            if(matrix[mid][0]==target) return true;
+            if(matrix[mid][0]>target) end=mid-1;
+            else start=mid+1;
+        }
+        int r=end;//targetRow
+        start=0;end=col-1;
+        while(start<=end){
+            mid=(start+end)/2;
+            if(matrix[r][mid]==target) return true;
+            if(matrix[r][mid]>target) end=mid-1;
+            else start=mid+1;
+        }
+        return false;
+        /**basic solution
+        int row=matrix.length,col=matrix[0].length;
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                 if(matrix[i][j]==target) return true;
             }
         }
         return false;
+        */
     }
 }
