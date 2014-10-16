ad4b6df94fb94ecc6db28fa4a674708af09bef7d
CharCollins <unique.zhangliang@gmail.com>
2014-10-16 15:56:48 +0800

    Update PalindromeNumber.java

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -8,5 +8,18 @@ public class PalindromeNumber {
             result=result*10+m;
         }
         return x==result;
+        /**
+        if(x<0) return false;
+        int div=1;
+        while(x/div>=10) div*=10;
+        while(x>0){
+            int l=x/div;
+            int r=x%10;
+            if(l!=r) return false;
+            x=x%div/10;
+            div/=100;//首尾已经比较，故div小100倍
+        }
+        return true;
+        */
     }
 }
